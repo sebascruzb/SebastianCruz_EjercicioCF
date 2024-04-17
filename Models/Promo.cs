17934ff70b236500d89a78256af449dde78b9405
Sebascruz <sebascruzb03@hotmail.com>
2024-04-17 11:33:57 -0500

    Creacion clase Promo

---
+++ Models/Promo.cs
@@ -0,0 +1,12 @@
+﻿namespace SebastianCruz_EjercicioCF.Models
+{
+    public class Promo
+    {
+        public int PromoID { get; set; }
+        public string? Description { get; set; }
+        public DateTime FechaPromo {  get; set; }
+
+        public int Id { get; set; }
+        public Burguer? Burguer { get; set; }
+    }
+}
